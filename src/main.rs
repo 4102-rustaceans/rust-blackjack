036dc23ba0c9fd7bc8b0d6277267669996a5310c
RJ Duane <git@rjd.io>
2018-02-16 17:38:25 -0500

    Add basic explanation of program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Rust Sample Project");
+    println!("by the ITCS 4102 Rustaceans: Richard Duane, Luke Frazier, and");
+    println!("");
+    println!("Demonstrating I/O, Control Structures, and Data Structures in Rust");
+}
+
+fn play() {
+    
 }
