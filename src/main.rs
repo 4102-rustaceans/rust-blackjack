391bdaa735065a3b77ff609bb776922e4869a567
RJ Duane <git@rjd.io>
2018-02-19 16:16:47 -0500

    Add stdin code to get input from user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 struct Card {
     suit: String,
     rank: String
@@ -13,6 +15,24 @@ fn main() {
     println!("by the ITCS 4102 Rustaceans: Richard Duane, Luke Frazier, and Seth Morris");
     println!("");
     println!("Demonstrating I/O, Control Structures, and Data Structures in Rust");
+
+    let mut input = String::new();
+    io:stdin().readline(&mut input).expect("Could not get input from stdin");
+
+    input = input.trim();
+
+    let mut choice = 0;
+
+    match input.parse::<u32>() {
+        Ok(i) => choice = i,
+        Err(e) => println!("Error: {}", e)
+    }
+
+    match choice {
+        1 => explain(),
+        2 => play(),
+        _ => println!("Invalid choic")
+    }
 }
 
 fn explain() {
@@ -25,18 +45,51 @@ fn play() {
 
     let mut dealer = Player{name: "Dealer", hand: Vec<Card>::new()};
     let mut player = Player{name: "Player", hand: Vec<Card>::new()};
+    let mut playerHold = false;
+    let mut dealerHold = false;
+    let mut playerBust = false;
+    let mut dealerBust = false;
 
     loop {
         // player's turn
+        if (!playerHold)
+        println!("Current hand value: {}", getHandValue(player.hand));
+        println!("If an Ace would cause you to go over 21, it will be automatically counted as a 1 instead of 11.");
+        println!("Cards in hand: \n{}", printHand(player.hand));
 
+        println!("What would you like to do?");
+        println!("1) Draw a card");
+        println!("2) Hold");
+
+        let mut input = String::new();
+        io:stdin().readline(&mut input).expect("Could not get input from stdin");
+
+        input = input.trim();
+
+        let mut choice = 0;
+
+        match input.parse::<u32>() {
+            Ok(i) => choice = i,
+            Err(e) => println!("Error: {}", e)
+        }
+
+        match choice {
+            1 => player.hand.push(deck.pop()),
+            2 => playerHold = true,
+            _ => println!("Invalid choice")
+        }
 
         // daeler's turn
         if getHandValue(dealer.hand) < 17 {
-            // don't draw another card, close enough
-        } else {
-            // draw a card
+            dealer.hand.push(deck.pop())
+        }
+
+        if (playerHold && dealerHold) || playerBust || dealerBust {
+            break;
         }
     }
+
+
 }
 
 fn createDeck() -> Vec<Card> {
@@ -87,4 +140,15 @@ fn getHandValue(hand: Vec<Card>) -> u32 {
     }
 
     value
+}
+
+fn printHand(hand: Vec<Card>) -> String {
+    let mut inHand: String = "";
+
+    for card in hand {
+        let mut curCard: String = format!("{} of {}", card.rank, card.suit);
+        inHand = format!("{}{}\n", inHand, curCard);
+    }
+
+    inHand
 }
